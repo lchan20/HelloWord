a25712d82bf322e021a26d59d4dad20c83abeaa9
LUCYC <lchan217@my.bcit.ca>
2020-01-07 13:58:26 -0800

    first commit

---
+++ Temperature.py
@@ -0,0 +1,4 @@
+temp_celsius = 10
+temp_fahr = (temp_celsius*9/5)+32
+print("The temperature in degrees Celsius is " + str(temp_celsius))
+print("The temperature in Fahrenheit is " + str(temp_fahr))
